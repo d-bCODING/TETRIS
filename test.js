c30005fecff5a71e27d7186e7b7c35d14093831c
d-bCODING <rlaehq@gmail.com>
2023-04-04 23:38:09 +0900

    좌우이동제한설정

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var boxes = document.querySelectorAll('.block-list li ul li');
-var columns = document.querySelectorAll('.block-list > li');
+var rows = document.querySelectorAll('.block-list > li');
 
 var click1 = document.querySelector('.column15 ul .row10');
 var click2 = document.querySelector('.column14 ul .row10');
@@ -12,31 +12,24 @@ var barLP = 4;
 
 //1자바
 var bar = [
-    columns[barTP].children[0].children[barLP],
-    columns[barTP + 1].children[0].children[barLP],
-    columns[barTP + 2].children[0].children[barLP],
-    columns[barTP + 3].children[0].children[barLP]
-]
-
-var shiftBar = [
-    columns[barTP].children[0].children[barLP - 2],
-    columns[barTP].children[0].children[barLP - 1],
-    columns[barTP].children[0].children[barLP],
-    columns[barTP].children[0].children[barLP + 1]
+    rows[barTP].children[0].children[barLP],
+    rows[barTP + 1].children[0].children[barLP],
+    rows[barTP + 2].children[0].children[barLP],
+    rows[barTP + 3].children[0].children[barLP]
 ]
 
 var barL = [
-    columns[barTP].children[0].children[barLP],
-    columns[barTP + 1].children[0].children[barLP],
-    columns[barTP + 2].children[0].children[barLP],
-    columns[barTP + 2].children[0].children[barLP + 1]
+    rows[barTP].children[0].children[barLP],
+    rows[barTP + 1].children[0].children[barLP],
+    rows[barTP + 2].children[0].children[barLP],
+    rows[barTP + 2].children[0].children[barLP + 1]
 ]
 
 var barL2 = [
-    columns[barTP].children[0].children[barLP],
-    columns[barTP + 1].children[0].children[barLP],
-    columns[barTP + 2].children[0].children[barLP],
-    columns[barTP + 2].children[0].children[barLP - 1]
+    rows[barTP].children[0].children[barLP],
+    rows[barTP + 1].children[0].children[barLP],
+    rows[barTP + 2].children[0].children[barLP],
+    rows[barTP + 2].children[0].children[barLP - 1]
 ]
 
 console.log(barL2);
@@ -60,7 +53,7 @@ for (let i = 0; i < barL2.length; i++) {
 var moving = document.querySelectorAll('.on');
 console.log(moving);
 
-//ㅣ자바를 기준으로 해당 변수들을 통해서 다른 모양들 생성
+//ㅣ자 도형을 기준으로 해당 변수들을 통해서 다른 모양들 생성
 var a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0;
 if (moving[3].innerHTML == "12 - 5") { //1자 도형
     b = 1,
@@ -71,17 +64,18 @@ if (moving[3].innerHTML == "12 - 5") { //1자 도형
         c = 2,
         d = 2,
         h = 1
-        console.log("dd");
-} else if (moving[2].innerHTML == "13 - 4") { //문제발생, moving[3] 이랑 barL2[3]이 서로 안겹침.
+}
+else if (moving[2].innerHTML == "13 - 4") { //문제발생, moving[3] 이랑 barL2[3]이 서로 안겹침.
     b = 1,
         c = 2,
         d = 2,
         h = -1
-        console.log("dd");
 }
+
 
 // 새로 생성된 도형 이후에 변경이나, 이동에 있어 새로 관여할 변수 생성
 var movingEl;
+
 //아래로 이동 버튼
 click1.addEventListener('click', function () {
     barTP++;
@@ -92,10 +86,10 @@ click1.addEventListener('click', function () {
     }
 
     movingEl = [
-        columns[barTP + a].children[0].children[barLP + e],
-        columns[barTP + b].children[0].children[barLP + f],
-        columns[barTP + c].children[0].children[barLP + g],
-        columns[barTP + d].children[0].children[barLP + h]
+        rows[barTP + a].children[0].children[barLP + e],
+        rows[barTP + b].children[0].children[barLP + f],
+        rows[barTP + c].children[0].children[barLP + g],
+        rows[barTP + d].children[0].children[barLP + h]
     ]
 
     for (let i = 0; i < movingEl.length; i++) {
@@ -106,46 +100,62 @@ click1.addEventListener('click', function () {
 //왼쪽으로 이동 버튼
 click2.addEventListener('click', function () {
     moving = document.querySelectorAll('.on');
+
+    var columns = [];
     for (let i = 0; i < moving.length; i++) {
-        moving[i].classList.remove('on');
+        columns[i] = moving[i].getAttribute('class').substring(3, 5);
     }
+    columns.sort((a, b) => a - b);
+    //현재 도형의 가장 왼쪽에 위치한 도형이 가진 열에 번호
+    var minLimit = columns[0];
 
-    barLP--;
-
-    movingEl = [
-        columns[barTP + a].children[0].children[barLP + e],
-        columns[barTP + b].children[0].children[barLP + f],
-        columns[barTP + c].children[0].children[barLP + g],
-        columns[barTP + d].children[0].children[barLP + h]
-    ]
-
-    for (let i = 0; i < movingEl.length; i++) {
-        movingEl[i].classList.add('on');
+    if (minLimit > 1) {
+        for (let i = 0; i < moving.length; i++) {
+            moving[i].classList.remove('on');
+        }
+        barLP--;
+        movingEl = [
+            rows[barTP + a].children[0].children[barLP + e],
+            rows[barTP + b].children[0].children[barLP + f],
+            rows[barTP + c].children[0].children[barLP + g],
+            rows[barTP + d].children[0].children[barLP + h]
+        ]
+        for (let i = 0; i < movingEl.length; i++) {
+            movingEl[i].classList.add('on');
+        }
     }
-
 })
 
 //오른쪽으로 이동 버튼
 click3.addEventListener('click', function () {
     moving = document.querySelectorAll('.on');
+
+    var columns = [];
     for (let i = 0; i < moving.length; i++) {
-        moving[i].classList.remove('on');
+        columns[i] = moving[i].getAttribute('class').substring(3, 5);
+        parseInt(columns[i]);
     }
-
-    barLP++;
-
-    movingEl = [
-        columns[barTP + a].children[0].children[barLP + e],
-        columns[barTP + b].children[0].children[barLP + f],
-        columns[barTP + c].children[0].children[barLP + g],
-        columns[barTP + d].children[0].children[barLP + h]
-    ]
-
-    for (let i = 0; i < movingEl.length; i++) {
-        movingEl[i].classList.add('on');
+    columns.sort((a, b) => a - b);
+    //현재 도형의 가장 오른쪽에 위치한 도형이 가진 열에 번호
+    var maxLimit = columns[columns.length - 1];
+    
+    if (maxLimit < 10) {
+        for (let i = 0; i < moving.length; i++) {
+            moving[i].classList.remove('on');
+        }
+        barLP++;
+        movingEl = [
+            rows[barTP + a].children[0].children[barLP + e],
+            rows[barTP + b].children[0].children[barLP + f],
+            rows[barTP + c].children[0].children[barLP + g],
+            rows[barTP + d].children[0].children[barLP + h]
+        ]
+        for (let i = 0; i < movingEl.length; i++) {
+            movingEl[i].classList.add('on');
+        }
     }
-
 })
+
 
 //모양 변경 버튼
 click4.addEventListener('click', function () {
@@ -154,6 +164,7 @@ click4.addEventListener('click', function () {
         moving[i].classList.remove('on');
     }
 
+    //각 도형별로 모양 재설정
     if (shape == 1) {
         a = a = 1;
         e = e - 1;
@@ -167,13 +178,19 @@ click4.addEventListener('click', function () {
         c = c - 1;
         g = g + 1;
         d = d - 2;
+    } else if (shape == 3) {
+        a = a + 1;
+        e = e + 1;
+        c = c - 1;
+        g = g - 1;
+        d = d - 2;
     }
 
     movingEl = [
-        columns[barTP + a].children[0].children[barLP + e],
-        columns[barTP + b].children[0].children[barLP + f],
-        columns[barTP + c].children[0].children[barLP + g],
-        columns[barTP + d].children[0].children[barLP + h]
+        rows[barTP + a].children[0].children[barLP + e],
+        rows[barTP + b].children[0].children[barLP + f],
+        rows[barTP + c].children[0].children[barLP + g],
+        rows[barTP + d].children[0].children[barLP + h]
     ]
 
     for (let i = 0; i < movingEl.length; i++) {
@@ -181,3 +198,6 @@ click4.addEventListener('click', function () {
     }
 
 })
+
+
+
